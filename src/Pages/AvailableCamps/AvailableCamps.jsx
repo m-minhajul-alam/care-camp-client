23a5b4db9295a8b876c0b91d16fd14bac48d0967
Minhajul-alam <alam.minhajul@outlook.com>
2023-11-24 19:12:43 +0600

    Added a responsive Availabke Camps page

--- src/Pages/AvailableCamps/AvailableCamps.jsx
+++ src/Pages/AvailableCamps/AvailableCamps.jsx
@@ -7,20 +7,12 @@ import {
   CardContent,
   Button,
   Grid,
-  TextField,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
 } from "@mui/material";
 
 const AvailableCamps = () => {
   const [camps, setCamps] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sortCriteria, setSortCriteria] = useState("mostRegistered");
 
   useEffect(() => {
-    // Fetch and set the camp data from your API or JSON file
     const fetchCamps = async () => {
       try {
         const response = await fetch("/campData.json");
@@ -34,31 +26,9 @@ const AvailableCamps = () => {
     fetchCamps();
   }, []);
 
-  // Function to handle the join camp action
   const joinCamp = (campId) => {
-    // Implement your logic for joining a camp, e.g., redirect to a registration page
     console.log(`Joining camp with ID: ${campId}`);
   };
-
-  // Filter and sort the camps based on search and sort criteria
-  const filteredCamps = camps.filter(
-    (camp) =>
-      camp.name && camp.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const sortedCamps = filteredCamps.sort((a, b) => {
-    // Implement sorting logic based on the selected criteria
-    switch (sortCriteria) {
-      case "mostRegistered":
-        return b.participantCount - a.participantCount;
-      case "targetAudience":
-        return a.targetAudience.localeCompare(b.targetAudience);
-      case "alphabeticalOrder":
-        return a.name.localeCompare(b.name);
-      default:
-        return 0;
-    }
-  });
 
   return (
     <Container sx={{ my: "68px" }}>
@@ -74,77 +44,65 @@ const AvailableCamps = () => {
       >
         Available Camps
       </Typography>
-
-      {/* Optional: Search functionality */}
-      <TextField
-        label="Search Camps"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-
-      {/* Optional: Sort by field */}
-      <FormControl fullWidth variant="outlined" margin="normal">
-        <InputLabel id="sortCriteriaLabel">Sort by</InputLabel>
-        <Select
-          labelId="sortCriteriaLabel"
-          id="sortCriteriaSelect"
-          value={sortCriteria}
-          label="Sort by"
-          onChange={(e) => setSortCriteria(e.target.value)}
-        >
-          <MenuItem value="mostRegistered">Most Registered</MenuItem>
-          <MenuItem value="targetAudience">Target Audience</MenuItem>
-          <MenuItem value="alphabeticalOrder">Alphabetical Order</MenuItem>
-          <MenuItem value="alphabeticalOrder">Open to all age groups</MenuItem>
-        </Select>
-      </FormControl>
-
-      {/* Camps list */}
       <Grid container spacing={3}>
-        {sortedCamps?.map((camp) => (
-          <Grid item key={camp.id} xs={12} sm={6} md={4} lg={4}>
-            <Card>
-              {/* Camp image */}
+        {camps?.map((camp) => (
+          <Grid
+            item
+            key={camp.id}
+            xs={12}
+            sm={6}
+            md={4}
+            lg={4}
+            sx={{ display: "flex", flexDirection: "column" }}
+          >
+            <Card
+              sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            >
               <img
                 src={camp.image}
-                alt={camp.name}
+                alt={camp.campName}
                 style={{ width: "100%", height: "200px", objectFit: "cover" }}
               />
-
-              <CardContent>
-                {/* Camp details */}
+              <CardContent sx={{ flexGrow: 1 }}>
                 <Typography variant="h5" gutterBottom>
-                  {camp.name}
+                  {camp.campName}
                 </Typography>
                 <Typography
                   variant="subtitle2"
                   color="textSecondary"
                   gutterBottom
                 >
-                  Date: {camp.scheduledDateTime}
+                  <strong>Date:</strong>{" "}
+                  {new Date(camp.scheduledDateTime).toLocaleString()}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" gutterBottom>
-                  Venue: {camp.venueLocation}
+                  <strong>Venue:</strong> {camp.venueLocation}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" gutterBottom>
-                  Target Audience: {camp.targetAudience}
+                  <strong>Target Audience:</strong> {camp.targetAudience}
                 </Typography>
-
-                {/* Join Camp button */}
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => joinCamp(camp.id)}
-                  component={Link}
-                  to={`/camps/${camp.id}`}
-                  sx={{ mt: 2 }}
-                >
-                  Join Camp
-                </Button>
+                <Typography variant="body2" color="textSecondary" gutterBottom>
+                  <strong>Specialized Services:</strong>{" "}
+                  {camp.specializedServices.join(", ")}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" gutterBottom>
+                  <strong>Healthcare Professionals:</strong>{" "}
+                  {camp.healthcareProfessionals.join(", ")}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" gutterBottom>
+                  <strong>Description:</strong> {camp.description}
+                </Typography>
               </CardContent>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => joinCamp(camp.id)}
+                component={Link}
+                to={`/camps/${camp.id}`}
+                sx={{ mt: "auto" }}
+              >
+                Join Camp
+              </Button>
             </Card>
           </Grid>
         ))}
